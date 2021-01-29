ccaca0b017c024c55ef2f9b6a4b6517856fa2b78
JensenBeard <jensenbeard00@gmail.com>
2021-01-29 17:10:47 +0000

    Major Progress
    Added player movement and collision with enviroment
    Added all Player animations

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    public Rigidbody2D rb;
+
+    public Animator anim;
+
+    public float jumpForce = 20f;
+    public Transform feet;
+    public LayerMask groundLayers;
+
+    float mx;
+
+    
+    private void Update() {
+        mx = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded()) {
+            Jump();
+        }
+
+        if (Mathf.Abs(mx) > 0.5f)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else {
+            anim.SetBool("isRunning", false);
+        }
+
+        if (mx > 0f)
+        {
+            transform.localScale = new Vector3(0.5f, 0.5f, 0f);
+        }
+        else if (mx < 0f)
+        {
+            transform.localScale = new Vector3(-0.5f, 0.5f, 0f);
+        }
+
+        anim.SetBool("isGrounded", IsGrounded());
+    }
+
+    private void FixedUpdate() {
+        Vector2 movement = new Vector2(mx * movementSpeed, rb.velocity.y);
+
+        rb.velocity = movement;
+    }
+
+    void Jump() {
+        Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
+        rb.velocity = movement;
+    }
+
+    public bool IsGrounded() {
+        Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 0.5f, groundLayers);
+
+        if (groundCheck != null) {
+            return true;
+        }
+        return false;
+    }
+}
